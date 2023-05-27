875ef28d9a283e33f715bdf31f8984722545624f
Simon <janssensimon3@gmail.com>
2023-05-26 20:44:41 -0400

    Asss singly linked list

--- structures.jl
+++ structures.jl
@@ -6,6 +6,18 @@ println("Hello world!")
 array = [1, 2, 3, 4]
 
 # Lists
+
 abstract type List{T} end
+abstract type Node{T} end
 
 ## Singly linked lists
+
+struct SinglyLinkedListNode{T} <: Node{T}
+    value::T
+    next::Union{SinglyLinkedListNode{T}, Nothing}
+end
+
+struct SinglyLinkedList{T} <: List{T}
+    length::Int
+    head::Union{SinglyLinkedListNode{T}, Nothing}
+end
